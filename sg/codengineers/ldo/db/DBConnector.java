e530029cb5bf30e41639e7d7073682197bef88c0
kaikj <bluesword246@gmail.com>
2014-10-01 09:41:59 +0800

    Define member variables

--- sg/codengineers/ldo/db/DBConnector.java
+++ sg/codengineers/ldo/db/DBConnector.java
@@ -6,6 +6,10 @@ import sg.codengineers.ldo.model.Task;
 
 public class DBConnector {
 	
+
+	private String filename;
+	private File file;
+
 	public ArrayList<Task> retrieveTaskList(){
 		return null;
 	}
