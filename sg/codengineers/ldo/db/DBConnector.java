2778bf27ce7b74c5c1316734a802f5a22214b56c
kaikj <bluesword246@gmail.com>
2014-10-01 09:42:36 +0800

    Constructor for database

--- sg/codengineers/ldo/db/DBConnector.java
+++ sg/codengineers/ldo/db/DBConnector.java
@@ -5,10 +5,18 @@ import java.util.ArrayList;
 import sg.codengineers.ldo.model.Task;
 
 public class DBConnector {
-	
 
 	private String filename;
 	private File file;
+
+	public DBConnector(String filename) {
+		this.filename = filename;
+		file = new File(filename);
+
+		if (!file.exists()) {
+			file.createNewFile();
+		}
+	}
 
 	public ArrayList<Task> retrieveTaskList(){
 		return null;
