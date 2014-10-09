4d056db6217d3da78587403ac4d477dfe5c1ee66
kaikj <bluesword246@gmail.com>
2014-10-10 01:25:46 +0800

    Skeleton code for the textfile connector

---
+++ sg/codengineers/ldo/db/TextDBConnector.java
@@ -0,0 +1,14 @@
+package sg.codengineers.ldo.db;
+
+/**
+ * This class is the implementation of the DBConnector
+ * interface. Specifically, it is the connection between
+ * the program and a textfile representation of the data
+ * in the program.
+ * 
+ * @author Sean
+ */
+
+public class TextDBConnector implements DBConnector {
+	
+}
