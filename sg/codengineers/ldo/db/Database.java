056f29e74c42440f10abba3a460c803630d2bf8e
kaikj <bluesword246@gmail.com>
2014-10-10 01:19:55 +0800

    Skeleton code for the database abstraction layer

---
+++ sg/codengineers/ldo/db/Database.java
@@ -0,0 +1,16 @@
+package sg.codengineers.ldo.db;
+
+import java.util.*;
+
+/**
+ * This class is the database abstraction layer that sits on top of the data
+ * layer. It manages how data is handled within the program, decides where
+ * to save all data and how to go about doing it. It chooses the appropriate
+ * connector which saves the data diligently. All changes to the data must go
+ * through this class
+ *
+ * @author Sean
+ */
+public class Database {
+
+}
