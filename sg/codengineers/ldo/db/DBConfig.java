22d0ab5f425cc8ef627cc16dcdbe324b179c9fd2
kaikj <bluesword246@gmail.com>
2014-10-10 01:17:05 +0800

    Updated description

--- sg/codengineers/ldo/db/DBConfig.java
+++ sg/codengineers/ldo/db/DBConfig.java
@@ -3,7 +3,8 @@ package sg.codengineers.ldo.db;
 /**
  * This model defines the database configurations of the
  * program, including which model class has access to which
- * type of database. It also helps to initialize all the 
+ * type of database. Each object represents one type of
+ * configuration. It also helps to initialize all the 
  * database connections that the program has. This model is
  * special since it has direct connection to the database. 
  * This is done since initializing the database requires
