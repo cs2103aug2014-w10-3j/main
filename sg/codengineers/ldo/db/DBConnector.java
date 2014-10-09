03c8caeef327b4524a95db50162792eb1fabd9ee
kaikj <bluesword246@gmail.com>
2014-10-10 00:08:53 +0800

    Use DBConnector as an interface to the actual connector for abstraction purposes

--- sg/codengineers/ldo/db/DBConnector.java
+++ sg/codengineers/ldo/db/DBConnector.java
@@ -1,96 +1,15 @@
 package sg.codengineers.ldo.db;
 
 /**
- * This class is the connector between the text file and
- * the database abstraction layer.
+ * This class is the connector between the database
+ * implementation and the database abstraction layer
  *
  * @author Sean
  */
 
-import java.text.DateFormat;
-import java.text.ParseException;
 import java.util.*;
-import java.io.*;
 
-import sg.codengineers.ldo.logic.TaskImpl;
-import sg.codengineers.ldo.model.Task;
+public interface DBConnector {
 
-public class DBConnector {
 
-	private String filename;
-	private File file;
-
-	/**
-	 * Constructor
-	 * @param filename The name of the file to save the data to
-	 */
-	public DBConnector(String filename) {
-		this.filename = filename;
-		initFile();
-	}
-
-	/**
-	 * Initializes the file to make sure it exist before any IO
-	 * operations are performed on it. Create a new file with the
-	 * supplied filename if the file does not already exist
-	 */
-	private void initFile() {
-		try {
-			file = new File(filename);
-
-			if (!file.exists()) {
-				file.createNewFile();
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
-
-	public List<Task> read(){
-		try {
-			BufferedReader br = new BufferedReader(new FileReader(file));
-			String line;
-			List<Task> taskLst = new ArrayList<Task>();
-			while ((line = br.readLine()) != null) {
-				taskLst.add(parseLine(line));
-			}
-			br.close();
-			return taskLst;
-		} catch (IOException e) {
-			e.printStackTrace();
-			return null;
-		}
-	}
-
-	private Task parseLine(String line) {
-		String[] lineArr = line.trim().split(";");
-		Task t = new TaskImpl(Integer.parseInt(lineArr[0]), lineArr[1]);
-		t.setDescription(lineArr[2]);
-		try {
-			DateFormat df = DateFormat.getDateTimeInstance();
-			t.setTimeStart(df.parse(lineArr[3]));
-		} catch (ParseException e) {
-			e.printStackTrace();
-		}
-		return t;
-	}
-
-	public void write(Task task) {
-		try {
-			DateFormat df = DateFormat.getDateTimeInstance();
-			String line = task.getId() + ";";
-			line += task.getName() + ";";
-			line += task.getDescription() + ";";
-			line += df.format(task.getStartTime()) + ";";
-			line += df.format(task.getEndTime()) + ";";
-			line += task.getTag();
-
-			BufferedWriter bw = new BufferedWriter(new FileWriter(file, true));
-			bw.write(line);
-			bw.newLine();
-			bw.close();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
 }
