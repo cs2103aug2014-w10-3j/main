807cccf5813a5aa7801eb9e18d9c6d43c2a7b9dd
vhazali <victorhazali@gmail.com>
2014-10-11 21:22:29 +0800

    UI Class

---
+++ sg/codengineers/ldo/model/UI.java
@@ -0,0 +1,18 @@
+package sg.codengineers.ldo.model;
+
+/**
+ * This interface specifies all the available methods for the UI component
+ * 
+ * @author Victor Hazali
+ * 
+ */
+public interface UI {
+
+	public String readFromUser();
+
+	public void showToUser(Result result);
+
+	public void showToUser(String message);
+
+	public void showWelcomeMessage();
+}
