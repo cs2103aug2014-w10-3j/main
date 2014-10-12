5898f8985b5c4e717a3e8e8b5a923c6b6fac2913
vhazali <victorhazali@gmail.com>
2014-10-12 23:15:21 +0800

    Changed displayError to displayException

--- sg/codengineers/ldo/model/Output.java
+++ sg/codengineers/ldo/model/Output.java
@@ -20,13 +20,17 @@ public interface Output {
 	public void displayResult(Result result) throws Exception;
 
 	/**
-	 * Displays an error message to the user
+	 * Displays an exception message to the user. The method will first display
+	 * the message in the exception by calling the getMessage() method as
+	 * specified by the Exception Class. However, if a null is received, the
+	 * method will skip this step. The method will then execute
+	 * printStackTracte() method as specified by the Exception Class.
 	 * 
-	 * @param message
-	 *            The message to be show to the user. Should contain the reason
-	 *            behind the error
+	 * @param e
+	 *            The exception to be show to the user. Should contain the
+	 *            reason behind the exception.
 	 */
-	public void displayError(String message, Exception e);
+	public void displayException(Exception e);
 
 	/**
 	 * Displays the welcome message to the user
