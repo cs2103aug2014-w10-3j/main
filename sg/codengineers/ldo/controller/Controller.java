81a05cd0cedaa0f33763be1a4a78a76f532f5714
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-13 13:54:03 +0800

    Deleted cmd keyword show, kept retrieve

--- sg/codengineers/ldo/controller/Controller.java
+++ sg/codengineers/ldo/controller/Controller.java
@@ -62,8 +62,6 @@ public class Controller {
 			case RETRIEVE:
 				return logic.retrieveTask(command.getPrimaryOperand(),
 						command.getAdditionalArguments());
-			case SHOW:
-				return logic.showTasks(command.getAdditionalArguments());
 			default:
 				throw new Exception("Invalid command.");
 		}
