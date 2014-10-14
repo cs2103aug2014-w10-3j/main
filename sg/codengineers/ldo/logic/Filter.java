c8443919b73e7950bd603981f3a5390c74a0dbdd
LuanWH <lwhluan@gmail.com>
2014-10-14 21:02:40 +0800

    Add documentation for Filter and Handler

--- sg/codengineers/ldo/logic/Filter.java
+++ sg/codengineers/ldo/logic/Filter.java
@@ -1,5 +1,12 @@
 package sg.codengineers.ldo.logic;
 
+/**
+ * An interface that wraps a filter method to simulate "pass in functions as parameters".<br>
+ * You are strongly suggested to use anonymous classes to implement this interface.
+ * @author Wenhao
+ *
+ * @param <E> the type of input that {@link Filter#call(Object)} method checks.
+ */
 public interface Filter<E> {
 	public boolean call(E item);
 }
