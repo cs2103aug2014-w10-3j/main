dbc1d61cde5fe7d2b29e9799cb4438f8172dcf99
kaikj <bluesword246@gmail.com>
2014-10-19 22:17:03 +0800

    Skeleton code for google calendar integration

---
+++ sg/codengineers/ldo/db/GCalDBConnector.java
@@ -0,0 +1,32 @@
+package sg.codengineers.ldo.db;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GCalDBConnector implements DBConnector {
+
+	@Override
+	public void create(String data) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void update(String data) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public List<String> read() throws IOException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void delete(String data) throws IOException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
