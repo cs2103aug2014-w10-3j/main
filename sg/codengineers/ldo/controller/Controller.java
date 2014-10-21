df25c2317e1e9c0405335cb425e82b85a5df0239
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-21 16:11:38 +0800

    Added documentation

--- sg/codengineers/ldo/controller/Controller.java
+++ sg/codengineers/ldo/controller/Controller.java
@@ -10,23 +10,47 @@ import sg.codengineers.ldo.parser.CommandImpl;
 import sg.codengineers.ldo.ui.InputImpl;
 import sg.codengineers.ldo.ui.OutputImpl;
 
+/**
+ * Controller controls the execution of L'Do.
+ * It reads the user's input which contains a command,
+ * routes what to do with the data according to the
+ * command type, and prints the corresponding feedback.
+ */
 public class Controller {
-	// logic
+	//@author A0112171Y
+	
+	// Logic instance
 	private static Logic logic;
-	// ui
+	
+	// UI instances
 	private Input input;
 	private Output output;
-
+	
+	/**
+	 * Constructor
+	 */
 	public Controller(){
 		logic = Logic.getInstance();
 		input = new InputImpl();
 		output = new OutputImpl();
 	}
 	
+	/**
+	 * Controller is a singleton.
+	 * This function generates one controller instance.
+	 * 
+	 * @return one controller instance
+	 */
 	public static Controller getControllerInstance(){
 		return new Controller();
 	}
-
+	
+	/**
+	 * This function controls the execution of L'Do,
+	 * from reading the user's input, passing it to the
+	 * appropriate functions, and printing the feedback
+	 * to the user.
+	 */
 	public void run() {
 		try {
 			Command welcomeCommand, command;
@@ -50,7 +74,16 @@ public class Controller {
 			output.displayException(e);
 		}
 	}
-
+	
+	/**
+	 * This function is the control flow of data in L'Do.
+	 * It passes data to be processed by the Logic component
+	 * according to the command type.
+	 * 
+	 * @param command contains the command type and data to be processed
+	 * @return result contains the information needed for feedback
+	 * @throws Exception
+	 */
 	public Result executeCommand(Command command) throws Exception {
 		CommandType commandType = command.getCommandType();
 		switch (commandType) {
