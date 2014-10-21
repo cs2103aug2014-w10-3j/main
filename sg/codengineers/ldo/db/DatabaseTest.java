f13f3b8ce2531e304aa6bc2156358c22e0373a69
kaikj <bluesword246@gmail.com>
2014-10-22 04:49:55 +0800

    Define tasks to do before and after each test

--- sg/codengineers/ldo/db/DatabaseTest.java
+++ sg/codengineers/ldo/db/DatabaseTest.java
@@ -5,6 +5,20 @@ import org.junit.*;
 import java.util.*;
 
 public class DatabaseTest {
+	private static Database db;
+	
+	private static final int FIRST = 0;
+	
+	@Before
+	public void initDatabases() {
+		db = Database.initDatabase();
+		db.clear("Test");
+	}
+	
+	@After
+	public void clearDatabases() {
+		db.clear("Test");
+	}
 
 	@Test
 	public void testCreate() {
