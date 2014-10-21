13720d968909c847a0c2d4f6cd4163e75141d5fe
kaikj <bluesword246@gmail.com>
2014-10-21 15:10:24 +0800

    Comments for class

--- sg/codengineers/ldo/db/GCalDBConnector.java
+++ sg/codengineers/ldo/db/GCalDBConnector.java
@@ -1,5 +1,14 @@
 package sg.codengineers.ldo.db;
 
+/**
+ * This class is the implementation of the DBConnector
+ * interface. Specifically, it is the connection between
+ * the program and google calendar. It handles the synchronization 
+ * of data between the program and google calendar.
+ * 
+ * @author Sean
+ */
+
 import java.io.IOException;
 import java.util.List;
 
