71bf9226937bca2305ce732426d501e3ea9379db
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-22 00:05:25 +0800

    Created unit test for Controller

---
+++ sg/codengineers/ldo/controller/ControllerUnitTest.java
@@ -0,0 +1,55 @@
+package sg.codengineers.ldo.controller;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import sg.codengineers.ldo.model.Command;
+import sg.codengineers.ldo.model.Result;
+import sg.codengineers.ldo.model.Command.CommandType;
+import sg.codengineers.ldo.parser.CommandImpl;
+
+public class ControllerUnitTest {
+	boolean stub = true;
+	Controller controller = new Controller(stub);
+	@Test
+	public void testCreate() {
+		try {
+			Command command = new CommandImpl("add test1");
+			Result result = controller.executeCommand(command);
+			assertEquals(CommandType.CREATE, result.getCommandType());
+		} catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	public void testDelete() {
+		try {
+			Command command = new CommandImpl("delete test1");
+			Result result = controller.executeCommand(command);
+			assertEquals(CommandType.DELETE, result.getCommandType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void testUpdate() {
+		try {
+			Command command = new CommandImpl("update test1");
+			Result result = controller.executeCommand(command);
+			assertEquals(CommandType.UPDATE, result.getCommandType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void testRetrieve(){
+		try {
+			Command command = new CommandImpl("show test1");
+			Result result = controller.executeCommand(command);
+			assertEquals(CommandType.RETRIEVE, result.getCommandType());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
