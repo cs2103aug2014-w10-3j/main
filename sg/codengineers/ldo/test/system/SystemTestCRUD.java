f8753e2b94413c6938fe68d83d2b75428858c0d4
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-29 07:34:44 +0800

    Created JUnit file for system test

---
+++ sg/codengineers/ldo/test/system/SystemTestCRUD.java
@@ -0,0 +1,27 @@
+package sg.codengineers.ldo.test.system;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SystemTestCRUD {
+	@Test
+	public void testCreate() {
+		
+	}
+	
+	@Test
+	public void testRetrive() {
+		
+	}
+	
+	@Test
+	public void testUpdate() {
+		
+	}
+	
+	@Test
+	public void testDelete() {
+		
+	}
+}
