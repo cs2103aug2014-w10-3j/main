97e42435f7ee735db86e912ae7f6ce32c8f12ff1
unlimitedsky <v.sharon.lynn@gmail.com>
2014-10-29 09:17:31 +0800

    Tested create primary argument

--- sg/codengineers/ldo/test/system/SystemTestCRUD.java
+++ sg/codengineers/ldo/test/system/SystemTestCRUD.java
@@ -2,12 +2,35 @@ package sg.codengineers.ldo.test.system;
 
 import static org.junit.Assert.*;
 
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 
+import sg.codengineers.ldo.controller.Controller;
+
 public class SystemTestCRUD {
+	private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+	
+	@Before
+	public void setUpStreams() {
+		System.setOut(new PrintStream(outContent));
+	}
+	
+	@After
+	public void cleanUpStream() {
+		System.setOut(null);
+	}
+	
 	@Test
 	public void testCreate() {
+		Controller controller = new Controller();
 		
+		//only primary argument
+		controller.processString("add dummy task");
+		assertEquals("Added create task #1\n", outContent.toString());
 	}
 	
 	@Test
