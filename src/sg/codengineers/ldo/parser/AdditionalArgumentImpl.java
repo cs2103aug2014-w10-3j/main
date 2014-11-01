e853520122b0d0ffef94947e37d8a4f62b04892c
vhazali <victorhazali@gmail.com>
2014-11-01 22:30:14 +0800

    Changed constructor for AdditionalArgumentImpl

--- src/sg/codengineers/ldo/parser/AdditionalArgumentImpl.java
+++ src/sg/codengineers/ldo/parser/AdditionalArgumentImpl.java
@@ -1,8 +1,5 @@
 package sg.codengineers.ldo.parser;
 
-import java.util.Map;
-import java.util.TreeMap;
-
 import sg.codengineers.ldo.model.AdditionalArgument;
 
 /**
@@ -14,16 +11,13 @@ import sg.codengineers.ldo.model.AdditionalArgument;
  */
 public class AdditionalArgumentImpl implements AdditionalArgument {
 
-	/* Static Variables */
-	private static Map<String, ArgumentType>	_argsMap;
-
 	/* Member variables */
 	private ArgumentType						_argumentType;
 	private String								_operand;
 
 	/* Constructors */
-	public AdditionalArgumentImpl(String argumentType, String value) {
-		_argumentType = getArgumentType(argumentType);
+	public AdditionalArgumentImpl(ArgumentType argumentType, String value) {
+		_argumentType = argumentType;
 		_operand = value;
 	}
 
@@ -45,22 +39,5 @@ public class AdditionalArgumentImpl implements AdditionalArgument {
 	 */
 	public ArgumentType getArgumentType() {
 		return _argumentType;
-	}
-
-	/* Private Methods */
-
-	/**
-	 * Takes the input of the user and returns the argument type
-	 * 
-	 * @param argument
-	 *            Input string from user
-	 * @return The argument type of the argument.
-	 */
-	private ArgumentType getArgumentType(String argument) {
-		ArgumentType argumentType = _argsMap.get(argument);
-		if (argumentType == null) {
-			return ArgumentType.INVALID;
-		}
-		return argumentType;
 	}
 }
