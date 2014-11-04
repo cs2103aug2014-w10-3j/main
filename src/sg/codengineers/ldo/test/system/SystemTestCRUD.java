fe069bf0074812ebdbe5ed5ac51fc66440dc1207
unlimitedsky <v.sharon.lynn@gmail.com>
2014-11-04 19:14:54 +0800

    Updated update, added delete
    Delete check corner case first and last index

--- src/sg/codengineers/ldo/test/system/SystemTestCRUD.java
+++ src/sg/codengineers/ldo/test/system/SystemTestCRUD.java
@@ -99,6 +99,8 @@ public class SystemTestCRUD {
 			controller.processString("add task 3");
 			controller.processString("add task 4");
 			outContent.reset();
+			
+			//show (all)
 			controller.processString("show");
 			String expectedFeedback = "Showing\n"
 					+ "1. task 1\n"
@@ -106,6 +108,12 @@ public class SystemTestCRUD {
 					+ "3. task 3\n"
 					+ "4. task 4\n";
 			assertEquals(expectedFeedback, outContent.toString());
+			outContent.reset();
+			
+			//show all
+			controller.processString("show all");
+			assertEquals(expectedFeedback, outContent.toString());
+			outContent.reset();
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
@@ -115,6 +123,8 @@ public class SystemTestCRUD {
 	public void testUpdate() {
 		Controller controller = new Controller();
 		try{
+			//delete all tasks
+			
 			controller.processString("add task 1");
 			controller.processString("add task 2");
 			controller.processString("add task 3");
@@ -123,11 +133,12 @@ public class SystemTestCRUD {
 			
 			//update before show, will be from today's list shown at the beginning of the program
 			controller.processString("edit 1 -n task one");
-			assertEquals("Updated task one", outContent.toString());
+			assertEquals("Invalid index. Please view the latest list first.", outContent.toString());
 			outContent.reset();
 			
 			//update name
 			controller.processString("show");
+			outContent.reset();
 			controller.processString("update 1 -n task one");
 			assertEquals("Updated task one", outContent.toString());
 			outContent.reset();
@@ -138,6 +149,36 @@ public class SystemTestCRUD {
 	
 	@Test
 	public void testDelete() {
-		
+		Controller controller = new Controller();
+		try{
+			//delete all tasks
+			
+			controller.processString("add task 1");
+			controller.processString("add task 2");
+			controller.processString("add task 3");
+			controller.processString("add task 4");
+			outContent.reset();
+			
+			//delete before show, will be from today's list shown at the beginning of the program
+			controller.processString("delete 1");
+			assertEquals("Invalid index. Please view the latest list first.", outContent.toString());
+			outContent.reset();
+			
+			//[corner case] delete first index
+			controller.processString("show");
+			outContent.reset();
+			controller.processString("delete 1");
+			assertEquals("Deleted task 1", outContent.toString());
+			outContent.reset();
+			
+			//[corner case] delete last index
+			controller.processString("show");
+			outContent.reset();
+			controller.processString("delete 3");
+			assertEquals("Deleted task 4", outContent.toString());
+			outContent.reset();
+		} catch (Exception e){
+			e.printStackTrace();
+		}
 	}
 }
