01ecf0832cc042069eb7f76e7dfda8186d015188
vhazali <victorhazali@gmail.com>
2014-11-06 00:14:45 +0800

    Removed Exception from displayResult method.

--- src/sg/codengineers/ldo/ui/OutputImpl.java
+++ src/sg/codengineers/ldo/ui/OutputImpl.java
@@ -48,7 +48,7 @@ public class OutputImpl implements Output {
 	 *             the result is INVALID
 	 */
 	@Override
-	public void displayResult(Result result) throws Exception {
+	public void displayResult(Result result) {
 		_result = result;
 		_taskItr = result.getTasksIterator();
 		CommandType commandType = _result.getCommandType();
