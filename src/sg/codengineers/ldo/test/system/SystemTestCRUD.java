2904c01df1a559b8f15d26e391b3b6af9681a343
unlimitedsky <v.sharon.lynn@gmail.com>
2014-11-10 09:41:13 +0800

    Changed processString to processCommand in response to changes in Controller method

--- src/sg/codengineers/ldo/test/system/SystemTestCRUD.java
+++ src/sg/codengineers/ldo/test/system/SystemTestCRUD.java
@@ -30,52 +30,52 @@ public class SystemTestCRUD {
 		
 		try{
 			//only primary argument
-			controller.processString("add primary arg");
+			controller.processCommand("add primary arg");
 			assertEquals("Added primary arg\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + time using --time
-			controller.processString("add primary arg and time 1 --time 29/10/2014");
+			controller.processCommand("add primary arg and time 1 --time 29/10/2014");
 			assertEquals("Added primary arg and time 1\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + time using -t
-			controller.processString("add primary arg and time 2 -t 29/10/2014");
+			controller.processCommand("add primary arg and time 2 -t 29/10/2014");
 			assertEquals("Added primary arg and time 2\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + deadline using --deadline
-			controller.processString("add primary arg and deadline 1 --deadline 01/11/2014");
+			controller.processCommand("add primary arg and deadline 1 --deadline 01/11/2014");
 			assertEquals("Added primary arg and deadline 1\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + deadline using -d
-			controller.processString("add primary arg and deadline 2 -d 01/11/2014");
+			controller.processCommand("add primary arg and deadline 2 -d 01/11/2014");
 			assertEquals("Added primary arg and deadline 2\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + description using --description
-			controller.processString("add primary arg and desc 1 --description empty description");
+			controller.processCommand("add primary arg and desc 1 --description empty description");
 			assertEquals("Added primary arg and desc 1\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + description using -desc
-			controller.processString("add primary arg and desc 2 -desc empty description");
+			controller.processCommand("add primary arg and desc 2 -desc empty description");
 			assertEquals("Added primary arg and desc 2\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + description using --information
-			controller.processString("add primary arg and desc 3 --information empty description");
+			controller.processCommand("add primary arg and desc 3 --information empty description");
 			assertEquals("Added primary arg and desc 3\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + description using -info
-			controller.processString("add primary arg and desc 4 -info empty description");
+			controller.processCommand("add primary arg and desc 4 -info empty description");
 			assertEquals("Added primary arg and desc 4\n", outContent.toString());
 			outContent.reset();
 			
 			//primary argument + description using -a
-			controller.processString("add primary arg and desc 5 -a empty description");
+			controller.processCommand("add primary arg and desc 5 -a empty description");
 			assertEquals("Added primary arg and desc 5\n", outContent.toString());
 			outContent.reset();
 		} catch (Exception e) {
@@ -90,18 +90,18 @@ public class SystemTestCRUD {
 			//IMPT! delete all tasks
 			
 			//show all
-			controller.processString("show");
+			controller.processCommand("show");
 			assertEquals("Task list is empty.\n", outContent.toString());
 			outContent.reset();
 			
-			controller.processString("add task 1");
-			controller.processString("add task 2");
-			controller.processString("add task 3");
-			controller.processString("add task 4");
+			controller.processCommand("add task 1");
+			controller.processCommand("add task 2");
+			controller.processCommand("add task 3");
+			controller.processCommand("add task 4");
 			outContent.reset();
 			
 			//show (all)
-			controller.processString("show");
+			controller.processCommand("show");
 			String expectedFeedback = "Showing\n"
 					+ "1. task 1\n"
 					+ "2. task 2\n"
@@ -111,7 +111,7 @@ public class SystemTestCRUD {
 			outContent.reset();
 			
 			//show all
-			controller.processString("show all");
+			controller.processCommand("show all");
 			assertEquals(expectedFeedback, outContent.toString());
 			outContent.reset();
 		} catch (Exception e) {
@@ -125,21 +125,21 @@ public class SystemTestCRUD {
 		try{
 			//delete all tasks
 			
-			controller.processString("add task 1");
-			controller.processString("add task 2");
-			controller.processString("add task 3");
-			controller.processString("add task 4");
+			controller.processCommand("add task 1");
+			controller.processCommand("add task 2");
+			controller.processCommand("add task 3");
+			controller.processCommand("add task 4");
 			outContent.reset();
 			
 			//update before show, will be from today's list shown at the beginning of the program
-			controller.processString("edit 1 -n task one");
+			controller.processCommand("edit 1 -n task one");
 			assertEquals("Invalid index. Please view the latest list first.", outContent.toString());
 			outContent.reset();
 			
 			//update name
-			controller.processString("show");
+			controller.processCommand("show");
 			outContent.reset();
-			controller.processString("update 1 -n task one");
+			controller.processCommand("update 1 -n task one");
 			assertEquals("Updated task one", outContent.toString());
 			outContent.reset();
 		} catch (Exception e){
@@ -153,28 +153,28 @@ public class SystemTestCRUD {
 		try{
 			//delete all tasks
 			
-			controller.processString("add task 1");
-			controller.processString("add task 2");
-			controller.processString("add task 3");
-			controller.processString("add task 4");
+			controller.processCommand("add task 1");
+			controller.processCommand("add task 2");
+			controller.processCommand("add task 3");
+			controller.processCommand("add task 4");
 			outContent.reset();
 			
 			//delete before show, will be from today's list shown at the beginning of the program
-			controller.processString("delete 1");
+			controller.processCommand("delete 1");
 			assertEquals("Invalid index. Please view the latest list first.", outContent.toString());
 			outContent.reset();
 			
 			//[corner case] delete first index
-			controller.processString("show");
+			controller.processCommand("show");
 			outContent.reset();
-			controller.processString("delete 1");
+			controller.processCommand("delete 1");
 			assertEquals("Deleted task 1", outContent.toString());
 			outContent.reset();
 			
 			//[corner case] delete last index
-			controller.processString("show");
+			controller.processCommand("show");
 			outContent.reset();
-			controller.processString("delete 3");
+			controller.processCommand("delete 3");
 			assertEquals("Deleted task 4", outContent.toString());
 			outContent.reset();
 		} catch (Exception e){
